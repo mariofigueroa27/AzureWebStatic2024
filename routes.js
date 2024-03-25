9d2d6066bb3ae6b967668f2df2795725814f6397
mariofigueroa27 <mariofigueroa209@outlook.com>
2024-03-25 16:26:13 -0500

    cambios

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const route = (event) => {
 };
 
 const routes = {
-    "/": "/index.html",
+    "/": "https://black-river-0eee0a010.4.azurestaticapps.net/index.html",
     "/pages1": "https://black-river-0eee0a010.4.azurestaticapps.net/Page1/index1.html",
     "/pages2": "https://black-river-0eee0a010.4.azurestaticapps.net/Page2/indexotherpage.html",
 };
