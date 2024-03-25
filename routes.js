93cf765d81079ee9b2d54524122c9e1433fccaac
mariofigueroa27 <mariofigueroa209@outlook.com>
2024-03-25 16:41:29 -0500

    cambios

--- routes.js
+++ routes.js
@@ -15,7 +15,7 @@ const handleLocation = async () => {
     const path = window.location.pathname;
     const route = routes[path] || routes[404];
     const html = await fetch(route).then((data) => data.text());
-    document.getElementById("container").innerHTML = html;
+    document.getElementById("center-container").innerHTML = html;
 };
 
 window.onpopstate = handleLocation;
