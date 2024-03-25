5b8348651b145bd6011ddb472de19b482a584df3
mariofigueroa27 <mariofigueroa209@outlook.com>
2024-03-25 16:35:21 -0500

    cambios

--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const route = (event) => {
     event = event || window.event;
     event.preventDefault();
     window.history.pushState({}, "", event.target.href);
-    handleLocation();
 };
 
 const routes = {
@@ -11,14 +10,8 @@ const routes = {
     "/pages2": "https://black-river-0eee0a010.4.azurestaticapps.net/Page2/indexotherpage.html",
 };
 
-const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
-    const html = await fetch(route).then((data) => data.text());
-    document.getElementById("main-page").innerHTML = html;
-};
+
 
 window.onpopstate = handleLocation;
 window.route = route;
 
-handleLocation();
